ab30a76b49c77f77de0e6f553ff8028bfe5d7b72
Farid Zandi <Faridzs76@gmail.com>
2021-04-20 01:39:08 -0400

    fixed cifar100

--- DataLoaders/CIFAR100.py
+++ DataLoaders/CIFAR100.py
@@ -4,7 +4,7 @@ import torchvision
 import torchvision.transforms as transforms
 
 
-def CIFAR100(batch_size=50, output_size=32):
+def CIFAR10(batch_size=200, output_size=32):
     transform = transforms.Compose(
         [transforms.Resize(output_size),
          transforms.ToTensor(),
@@ -13,16 +13,21 @@ def CIFAR100(batch_size=50, output_size=32):
     image_datasets = {}
     dataloaders = {}
 
-    image_datasets['train'] = torchvision.datasets.CIFAR100(root='./data', train=True,
+    train_set = torchvision.datasets.CIFAR100(root='./data', train=True,
+                                              download=True, transform=transform)
+
+    image_datasets['test'] = torchvision.datasets.CIFAR100(root='./data', train=False,
                                                            download=True, transform=transform)
+
+    image_datasets['train'], image_datasets['val'] = torch.utils.data.random_split(train_set, [45000, 5000])
+
     dataloaders['train'] = torch.utils.data.DataLoader(image_datasets['train'], batch_size=batch_size,
                                                        shuffle=True, num_workers=2)
-
-    image_datasets['val'] = torchvision.datasets.CIFAR100(root='./data', train=False,
-                                                         download=True, transform=transform)
     dataloaders['val'] = torch.utils.data.DataLoader(image_datasets['val'], batch_size=batch_size,
-                                                     shuffle=False, num_workers=2)
+                                                     shuffle=True, num_workers=2)
+    dataloaders['test'] = torch.utils.data.DataLoader(image_datasets['val'], batch_size=batch_size,
+                                                      shuffle=False, num_workers=2)
 
-    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val', 'test']}
 
     return image_datasets, dataloaders, dataset_sizes, 100
